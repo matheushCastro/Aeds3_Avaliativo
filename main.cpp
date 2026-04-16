c54f30698ba0f2587040d16b45aea5a52ad12882
Matheus Henrique <mhenrique182005@gmail.com>
2026-04-16 01:28:02 -0300

    Refactor insertion sort implementation
    Refactor insertion sort logic to ensure correct ordering and improve readability.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
 
-//Plinio Henrique, Matheus Henrique, Marcos Roberto
-
 void insertionSort(int v[], int n) {
     int i = 0;
 
     while (i < n) {
 
-       
         if (i == n - 1) {
             int chave = v[i];
             int j = i - 1;
@@ -17,40 +14,38 @@ void insertionSort(int v[], int n) {
                 v[j + 1] = v[j];
                 j--;
             }
-
             v[j + 1] = chave;
             break;
         }
 
-       
         int a = v[i];
         int b = v[i + 1];
-        cout<<"Par analisado: " << a << " e " << b << endl;
 
-       
+        cout << "Par analisado: " << a << " e " << b << endl;
+
+        // Garantir ordem: a <= b
         if (a > b) {
             int temp = a;
             a = b;
             b = temp;
         }
 
-       
+        // 🔹 Inserir primeiro o MENOR (a)
         int j = i - 1;
-        while (j >= 0 && v[j] > b) {
-            v[j + 1] = v[j];
-            j--;
-        }
-        v[j + 1] = b;
-
-       
-        j--;
         while (j >= 0 && v[j] > a) {
             v[j + 1] = v[j];
             j--;
         }
         v[j + 1] = a;
 
-       
+
+        j = i; // começa depois do a inserido
+        while (j >= 0 && v[j] > b) {
+            v[j + 1] = v[j];
+            j--;
+        }
+        v[j + 1] = b;
+
         i += 2;
     }
 }
@@ -58,12 +53,6 @@ void insertionSort(int v[], int n) {
 int main() {
     int v[] = {7, 2, 5, 4, 9, 1};
     int n = sizeof(v) / sizeof(v[0]);
-    
-    cout << "Vetor original: ";
-    for (int i = 0; i < n; i++) {
-        cout << v[i] << " ";
-    }
-    cout << endl;
 
     insertionSort(v, n);
 
